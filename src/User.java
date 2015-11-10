f8378ddcf5b1684cee7a8d3de05273315a25d31a
iruynarak <iruynarak@gmail.com>
2015-11-10 15:58:49 -0300

    mudando construtor do User (to do: contem um erro)

--- src/User.java
+++ src/User.java
@@ -1,7 +1,11 @@
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class User {
 	private HashMap<Integer, Object> attributesOfUser;
@@ -9,12 +13,16 @@ public class User {
 	
 	User(HashMap<Integer, Object> attributesOfUserType)
 	{
+		
 		Set<Integer> key = attributesOfUserType.keySet();
+		Integer[] attributesArray = (Integer[]) key.toArray();
 		for(int i = 0; i < key.size(); i++)
 		{
-			Object aux = attributesOfUserType.get(key[i]);
-			this.attributesOfUser.put(key, aux);
+			
+			Object aux = attributesOfUserType.get(attributesArray[i]);
+			this.attributesOfUser.put(attributesArray[i], aux);
 		}
+		
 	}
 	
 	void editAttribute(Integer attributeID, Object content)
