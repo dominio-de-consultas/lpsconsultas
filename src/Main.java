29686a55bca8991deb44c15032b96175863f7f4f
iruynarak <iruynarak@gmail.com>
2015-11-10 14:36:51 -0300

    estruturacao da main

--- src/Main.java
+++ src/Main.java
@@ -10,43 +10,57 @@ public class Main
 		Scanner scanner = new Scanner(System.in);
 		
 		String option;
-		System.out.println("Escolha a opção (IUT / IU)");
-		option = scanner.nextLine();
-		switch (option)
+		
+		Boolean flag = true;
+		while(flag)
 		{
-		case "IUT":
-			Boolean[] listOfAttributes = new Boolean[Attributes.values().length];
-					
-			System.out.println("Entre com o nome do tipo de usuario:");
-			String name = scanner.nextLine();
+			System.out.println("Escolha a opção (IUT / IU)");
+			option = scanner.nextLine();
+			option = option.toUpperCase();
 			
-			
-			System.out.println("Escolha quais atributos estarao no tipo de usuario(S/N)");
-			Attributes[] aux = Attributes.values();
-			for(int i = 0; i < Attributes.values().length ; i++)
+			switch (option)
 			{
-				System.out.println(aux[i].toString());
-				String attributeOption = scanner.nextLine();
-				if(attributeOption == "S")
-				{
-					listOfAttributes[i] = true;
-				}
-				else
-				{
-					listOfAttributes[i] = false;
-				}
+				case "IUT":
+					Boolean[] listOfAttributes = new Boolean[Attributes.values().length];
+							
+					System.out.println("Entre com o nome do tipo de usuario:");
+					String name = scanner.nextLine();
+					
+					
+					System.out.println("Escolha quais atributos estarao no tipo de usuario(S/N)");
+					Attributes[] aux = Attributes.values();
+					for(int i = 0; i < Attributes.values().length ; i++)
+					{
+						System.out.println(aux[i].toString());
+						String attributeOption = scanner.nextLine();
+						attributeOption = attributeOption.toUpperCase();
+						
+						if(attributeOption == "S")
+						{
+							listOfAttributes[i] = true;
+						}
+						else
+						{
+							listOfAttributes[i] = false;
+						}
+					}
+					schedulingSystem.createUserType(name, listOfAttributes);
+					break;
+	
+				case "IU":
+					//User user = userType.createUser();
+					;
+				case "END":
+					flag = false;
+				default:
+					break;
+				
 			}
-			schedulingSystem.createUserType(name, listOfAttributes);
-			break;
+			
 
-		case "IU":
-			//User user = userType.createUser();
-			;
-		case "END":
-			scanner.close();
-		default:
-			break;
 		}
+		scanner.close();
+
 		
 		
 	}
