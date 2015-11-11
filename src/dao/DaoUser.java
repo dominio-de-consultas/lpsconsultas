9b91f01bd5cb6f41f83c424813c1133ae9a3ba00
Eliton Scardin Perin <elitonperin@gmail.com>
2015-11-11 15:37:46 -0300

    Adicionando arquivos do hibernate.

---
+++ src/dao/DaoUser.java
@@ -0,0 +1,38 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Transaction;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import main.User;
+
+import util.HibernateHelper;
+
+public class DaoUser {
+	private Session session;
+	private Transaction transaction;
+	private Query query;
+	
+	public List select(){
+		List<User> lista = new ArrayList<>();
+		
+		
+		try{
+			this.session = HibernateHelper.getSessao();
+			transaction = session.beginTransaction();
+			query = session.createQuery("from User");
+			if(query.list() != null)
+				lista = query.list();
+			session.close();
+		}catch(Exception e){
+			session.close();
+			e.printStackTrace();
+		}
+		
+		return lista;
+	}
+
+}
---
+++ src/util/HibernateHelper.java
@@ -0,0 +1,35 @@
+package util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaUpdate;
+
+public class HibernateHelper {
+	private static SessionFactory fabricaSessao;
+    private static Configuration hibernateConfig;
+    // Estrutura static para garantir que a SessionFactory seja iniciada apenas uma vez
+    
+    static {
+        try {
+            hibernateConfig = new Configuration().configure("resources\\hibernate.cfg.xml");
+            fabricaSessao = hibernateConfig.buildSessionFactory();
+            atualizarBD();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    // Retorna a sessão
+    public static Session getSessao(){
+        return fabricaSessao.openSession();
+    }
+ 
+    // Atualiza o Schema do Banco de Dados
+    private static void atualizarBD(){
+        SchemaUpdate se = new SchemaUpdate(hibernateConfig);
+        se.execute(true, true);
+    }
+	
+	
+}
