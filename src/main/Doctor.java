5148aaf7e520a077b929ab9c13a8e85be016f182
iruynarak <iruynarak@gmail.com>
2015-11-11 19:21:50 -0300

    mudanca na classe Doctor[Adicionando os atributos do medico]

--- src/main/Doctor.java
+++ src/main/Doctor.java
@@ -5,21 +5,29 @@ import java.util.Date;
 
 public class Doctor
 {
-	private long pid;
+	private Object[] listOfAttributes;
+		
+	
 	public ArrayList<Schedule> listOfSchedules;
 	
 
-	public long getPid() {
-		return pid;
+	public long getCRM() {
+		return (long) this.listOfAttributes[DoctorAttributes.CRM.ordinal()];
 	}
 
-	public void setPid(long pid) {
-		this.pid = pid;
+	public void setCRM(long CRM) {
+		this.listOfAttributes[DoctorAttributes.CRM.ordinal()] = CRM;
 	}
 
-	public Doctor()
+	public Doctor(Object[] listOfAttributes)
 	{
 		this.listOfSchedules = new ArrayList<Schedule>();
+		
+		this.listOfAttributes = new Object[DoctorAttributes.values().length];
+		for(int i = 0; i < DoctorAttributes.values().length; i++)
+		{
+			this.listOfAttributes[i] = listOfAttributes[i];
+		}
 	}
 	
 	
---
+++ src/main/DoctorAttributes.java
@@ -0,0 +1,6 @@
+package main;
+
+public enum DoctorAttributes
+{
+	CRM, nome, CPF, dataDeNascimento, especialidade, email, rua, CEP, numero, bairro, municipio, estado, complemento, telefone 
+}
