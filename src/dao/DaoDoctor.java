7cbbcbfc38966aa4ef0dea009ca965e75052fd5b
Eliton Scardin Perin <elitonperin@gmail.com>
2015-11-12 15:37:05 -0300

    Daos

--- src/dao/DaoDoctor.java
+++ src/dao/DaoDoctor.java
@@ -3,64 +3,62 @@ package dao;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.Transaction;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import main.Doctor;
-import main.User;
 import util.HibernateHelper;
 
 public class DaoDoctor {
 	private Session session;
 	private Transaction transaction;
 	private Query query;
-	
-	public List select(){
+
+	public List select() {
 		List<Doctor> lista = new ArrayList<>();
-		
-		
-		try{
+
+		try {
 			this.session = HibernateHelper.getSessao();
 			transaction = session.beginTransaction();
 			query = session.createQuery("from Doctor");
-			if(query.list() != null)
+			if (query.list() != null)
 				lista = query.list();
 			session.close();
-		}catch(HibernateException e){
+		} catch (HibernateException e) {
 			session.close();
 			e.printStackTrace();
 		}
-		
+
 		return lista;
 	}
-	
-	public void insertUpdate(Doctor doctor){
-		try{
+
+	public void insertUpdate(Doctor doctor) {
+		try {
 			this.session = HibernateHelper.getSessao();
 			transaction = session.beginTransaction();
 			session.saveOrUpdate(doctor);
 			transaction.commit();
 			session.close();
 			System.out.println("Salvo");
-		}catch(HibernateException e){
+		} catch (HibernateException e) {
 			session.close();
 			e.printStackTrace();
 		}
 	}
 
-	public void delete(User user){
-        try{
-            this.session = HibernateHelper.getSessao();
-            transaction = session.beginTransaction();
-            session.delete(user);
-            transaction.commit();
-            session.close();
-        } catch (HibernateException e) {
-			session.close(); 
-            e.printStackTrace(); 
-        }
-    }
+	public void delete(Doctor doctor) {
+		try {
+			this.session = HibernateHelper.getSessao();
+			transaction = session.beginTransaction();
+			session.delete(doctor);
+			transaction.commit();
+			session.close();
+		} catch (HibernateException e) {
+			session.close();
+			e.printStackTrace();
+		}
+	}
 
 }
